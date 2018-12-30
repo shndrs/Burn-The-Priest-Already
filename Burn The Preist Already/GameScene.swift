217c1dde5698a802f1759a182beb2fd650c02d15
Sahand Raeisi <sahandraeisi@yahoo.com>
2018-12-30 11:42:10 +0330

    create project

---
+++ Burn The Preist Already/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  Burn The Preist Already
+//
+//  Created by Sahand Raeisi on 12/30/18.
+//  Copyright © 2018 Sahand Raeisi. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+    }
+}
---
+++ Burn The Preist Already/GameViewController.swift
@@ -0,0 +1,50 @@
+//
+//  GameViewController.swift
+//  Burn The Preist Already
+//
+//  Created by Sahand Raeisi on 12/30/18.
+//  Copyright © 2018 Sahand Raeisi. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
